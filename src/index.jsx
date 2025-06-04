2367bf2cfbe0d3e54cfdb56b2a78cd66f9be4ac7
skiedflakes <46420286+skiedflakes@users.noreply.github.com>
2025-06-04 16:21:42 +0800

    updates
    updates

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        hello world
+      </Text>
+    </View>
+  );
+};
+
+export default App;
